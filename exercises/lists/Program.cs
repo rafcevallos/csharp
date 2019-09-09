bdcc32d2c3aef6863805378963969cc97660bdb3
Raf Cevallos <rafael.cevallos81@gmail.com>
2019-09-09 15:54:14 -0500

    Planets and Spaceships completed

--- exercises/lists/Program.cs
+++ exercises/lists/Program.cs
@@ -14,13 +14,28 @@ namespace lists
             planetList.Add("Saturn");
 
             // 2. Create another List that contains that last two planets of our solar system.
-            List<string> lastPlanets = new List<string>() { "Uranus", "Pluto" };
+            List<string> lastPlanets = new List<string>() { "Uranus", "Neptune" };
+
             // 3. Combine the two lists by using AddRange ().
+            planetList.AddRange(lastPlanets);
+
             // 4. Use Insert () to add Earth, and Venus in the correct order.
+            planetList.Insert(1, "Venus");
+            planetList.Insert(2, "Earth");
+
             // 5. Use Add () again to add Pluto to the end of the list.
-            // 6. Now that all the planets are in the list, slice the list using GetRange () in order to extract the rocky planets into a new list called rockyPlanets.The rocky planets will remain in the original planets list.
+            planetList.Add("Pluto");
+
+            // 6. Now that all the planets are in the list, slice the list using GetRange () in order to extract the rocky planets
+            // into a new list called rockyPlanets.The rocky planets will remain in the original planets list.
+            List<string> rockyPlanets = planetList.GetRange(0, 4);
+
             // 7. Being good amateur astronomers, we know that Pluto is now a dwarf planet, so use the Remove () method to eliminate it from the end of planetList.
+            planetList.Remove("Pluto");
+
             Console.WriteLine(string.Join(", ", planetList));
+            Console.WriteLine(string.Join(", ", lastPlanets));
+            Console.WriteLine(string.Join(", ", rockyPlanets));
             // foreach (var planet in planetList) {
             //     Console.WriteLine (planet);
             // }
