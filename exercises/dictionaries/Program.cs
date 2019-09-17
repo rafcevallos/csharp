4c61efa9e128aef81498840f6b0d051cd92e810f
Raf Cevallos <rafael.cevallos81@gmail.com>
2019-09-17 13:51:23 -0500

    Practice: Dictionary of Words completed

--- exercises/dictionaries/Program.cs
+++ exercises/dictionaries/Program.cs
@@ -1,35 +1,90 @@
 ﻿using System;
 using System.Collections.Generic;
 
-namespace dictionaries {
-    class Program {
-        static void Main (string[] args) {
+namespace dictionaries
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
             /*
                 Create a dictionary with key value pairs to
                 represent words (key) and its definition (value)
             */
-            Dictionary<string, string> wordsAndDefinitions = new Dictionary<string, string> ();
+            Dictionary<string, string> wordsAndDefinitions = new Dictionary<string, string>();
 
             // Add several more words and their definitions
-            wordsAndDefinitions.Add ("Awesome", "The feeling of students when they are learning C#");
-            wordsAndDefinitions.Add ("Fantastic", "The feeling after reading an awesome comic");
-            wordsAndDefinitions.Add ("Womp Womp", "The feeling when something is a total bummer");
-            wordsAndDefinitions.Add ("RAWR", "The feeling whe you want to smash something");
+            wordsAndDefinitions.Add("Awesome", "The feeling of students when they are learning C#");
+            wordsAndDefinitions.Add("Fantastic", "The feeling after reading an awesome comic");
+            wordsAndDefinitions.Add("Womp Womp", "The feeling when something is a total bummer");
+            wordsAndDefinitions.Add("RAWR", "The feeling whe you want to smash something");
 
             /*
                 Use square brackets to get the definition of two of the
                 words and then output them to the console
             */
-            Console.WriteLine (wordsAndDefinitions.GetValueOrDefault ("Awesome"));
-            Console.WriteLine (wordsAndDefinitions.GetValueOrDefault ("Womp Womp"));
+            Console.WriteLine(wordsAndDefinitions["Awesome"]);
+            Console.WriteLine(wordsAndDefinitions["Womp Womp"]);
+            // OR
+            Console.WriteLine(wordsAndDefinitions.GetValueOrDefault("Awesome"));
+            Console.WriteLine(wordsAndDefinitions.GetValueOrDefault("Womp Womp"));
             /*
                 Below, loop over the wordsAndDefinitions dictionary to get the following output:
                     The definition of (WORD) is (DEFINITION)
                     The definition of (WORD) is (DEFINITION)
                     The definition of (WORD) is (DEFINITION)
             */
-            foreach (KeyValuePair<string, string> word in wordsAndDefinitions) {
-                Console.WriteLine ($"The definition of {word.Key} is {word.Value}.");
+            foreach (KeyValuePair<string, string> word in wordsAndDefinitions)
+            {
+                Console.WriteLine($"The definition of {word.Key} is {word.Value}.");
+            }
+
+            // Make a new list
+            List<Dictionary<string, string>> dictionaryOfWords = new List<Dictionary<string, string>>();
+
+            /*
+                You want to track the following about each word:
+                    word, definition, part of speech, example sentence
+
+                Example of one dictionary in the list:
+                {
+                    "word": "excited",
+                    "definition": "having, showing, or characterized by a heightened state of energy, enthusiasm, eagerness",
+                    "part of speech": "adjective",
+                    "example sentence": "I am excited to learn C#!"
+                }
+            */
+
+            // Create dictionary to represent a few words
+            Dictionary<string, string> excitedWord = new Dictionary<string, string>();
+
+            // Add each of the 4 bits of data about the word to the Dictionary
+            excitedWord.Add();
+
+            // Add Dictionary to your `dictionaryOfWords` list
+
+
+            // create another Dictionary and add that to the list
+
+
+            /*
+                Iterate your list of dictionaries and output the data, You can use the two foreach() loops below to help you start your iteration.
+
+                Example output for one word in the list of dictionaries:
+                    word: excited
+                    definition: having, showing, or characterized by a heightened state of energy, enthusiasm, eagerness
+                    part of speech: adjective
+                    example sentence: I am excited to learn C#!
+            */
+
+            // Iterate the List of Dictionaries
+            foreach ()
+            {
+                // Iterate the KeyValuePairs of the Dictionary
+                foreach ()
+                {
+                    Console.WriteLine($"{wordData.Key}: {wordData.Value}");
+                }
             }
         }
     }
